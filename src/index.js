e1fdde69fcb2bbfb7d7aa0b7a91a0acc0b66b9d8
CantarinoG <guilhermecantarino1709@gmail.com>
2022-12-02 20:24:41 -0300

    Added the page layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+import './styles/layout.css';
+import bg from './imgs/bg.png';
+
+const body = document.querySelector('body');
+
+body.innerHTML = `
+<header>
+    <span class="logo">Gastronomy Deluxe</span>
+    <nav>
+        <ul class="nav">
+            <li><a class="item" href="#">Home</a></li>
+            <li><a class="item" href="#">Menu</a></li>
+            <li><a class="item" href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<main class="content"></main>
+
+<footer>
+    <a href="https://www.freepik.com/free-photo/restaurant-interior_1243339.htm#query=luxury%20restaurant&position=0&from_view=keyword">Image by evening_tao &nbsp;</a> on Freepik
+</footer>
+`;
